846f8da7b18465642191967038f67003ea4ef4d0
harry <dengchao2056@126.com>
2017-03-13 17:40:47 +0800

    打包优化

--- src/merges.js
+++ src/merges.js
@@ -1,24 +1,24 @@
-import { isArray, union, partialRight, mergeWith } from 'lodash'
+import _ from 'lodash'
 
 // Array concat
 let merge = (...args) => {
   function customizer(objValue, srcValue) {
-    if (isArray(objValue)) {
+    if (_.isArray(objValue)) {
       return objValue.concat(srcValue);
     }
   }
-  let defaults = partialRight(mergeWith, customizer);
+  let defaults = _.partialRight(_.mergeWith, customizer);
   return defaults.apply(defaults, args)
 }
 
 // Array set
 let mergeSet = (...args) => {
   function customizer(objValue, srcValue) {
-    if (isArray(objValue)) {
-      return union(objValue, srcValue);
+    if (_.isArray(objValue)) {
+      return _.union(objValue, srcValue);
     }
   }
-  let defaults = partialRight(mergeWith, customizer);
+  let defaults = _.partialRight(_.mergeWith, customizer);
   return defaults.apply(defaults, args)
 }
 
